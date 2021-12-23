6e84c20e38d228578fcb951ab1610f7d59076229
Nick Arabadzhi <91465047+Nickolas-26@users.noreply.github.com>
2021-12-23 19:25:03 +0200

    finish-01-task-50procent-3task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </li>`;
+  })
+  .join('');
+refs.galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
-
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textArea: document.querySelector('.feedback-form textarea'),
+};
+// добавляем слушателя для формы и поля ввода текста
+refs.form.addEventListener('submit', formSubmit);
+// refs.textArea.addEventListener('input', throttle(textAreaInput, 1000));
+// создали пустой объект formData для того ,что бы туда записывались ключи и значения ( name: email , value : message)
+refs.form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value;
+  // сделать объекту метод Json ( strigify)
+  const formDataStr = JSON.stringify(formData);
+  // считываем инпуты с 2х полей ,формируем обьект  записываем в localeStorage
+  localStorage.setItem(STORAGE_KEY, formDataStr);
+  // при загрузке страницы проверить лежат ли данные в storageKey , парсим их и заполняем эти поля формы (инпуты)
+});
+// saveText();
+// пишем callBack(и) для наший функций
+function formSubmit(e) {
+  // запрещаем действия по-умолчанию (то бишь странница при отправке формы не перезагружается)
+  e.preventDefault();
+  e.currentTarget.reset();
+  // если мы отправили форму - то нужно ее ресетнуть(очистить поля) ,что бы она еще раз не появилась после отправки
+  localStorage.removeItem(STORAGE_KEY);
+  //   console.log('отправляем форму');
+}
+// в function textAreaInput создаем переменную textMassage и присваиваем ему значение события ,которое происходит в данный момент! то есть event.currentTarget.value
+// function textAreaInput(e) {
+//   const textMassage = refs.textArea.value; //помменял с e.currentTurget так как потерялся наш target в данным момент
+//   // задаем локал стореджу ключ и значение(то что пишется в данный момент)
+//   localStorage.setItem(STORAGE_KEY, textMassage);
+// }
+// получаем значение из localStorage
+// function saveText() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+//   // нам надо написать если наша savedMessage приводится к true то мы можем работать и значит там есть какие-то данные(которые введены были до этого)
+//   if (savedMessage) {
+//     //   обновляем DOM , берем texArea и записываем ей value
+//     refs.textArea.value = savedMessage;
+//   }
+// }
