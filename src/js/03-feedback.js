439ce6caa0d610ef7634a7c9bbe50784832f9054
Nick Arabadzhi <91465047+Nickolas-26@users.noreply.github.com>
2021-12-24 12:56:26 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -18,8 +18,19 @@ refs.form.addEventListener('input', e => {
   // считываем инпуты с 2х полей ,формируем обьект  записываем в localeStorage
   localStorage.setItem(STORAGE_KEY, formDataStr);
   // при загрузке страницы проверить лежат ли данные в storageKey , парсим их и заполняем эти поля формы (инпуты)
+  function saveText() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    // нам надо написать если наша savedMessage приводится к true то мы можем работать и значит там есть какие-то данные(которые введены были до этого)
+    if (savedMessage === true) {
+      //   обновляем DOM , берем texArea и записываем ей value
+      refs.textArea.value = savedMessage;
+      savedMessage = localStorage.setItem(STORAGE_KEY, textMassage);
+    }
+  }
+
+  // где вызывать наш saveText
 });
-// saveText();
+//  saveText();
 // пишем callBack(и) для наший функций
 function formSubmit(e) {
   // запрещаем действия по-умолчанию (то бишь странница при отправке формы не перезагружается)
@@ -31,16 +42,8 @@ function formSubmit(e) {
 }
 // в function textAreaInput создаем переменную textMassage и присваиваем ему значение события ,которое происходит в данный момент! то есть event.currentTarget.value
 // function textAreaInput(e) {
-//   const textMassage = refs.textArea.value; //помменял с e.currentTurget так как потерялся наш target в данным момент
+//   const textMassage = refs.textArea.value; //поменял с e.currentTurget так как потерялся наш target в данным момент
 //   // задаем локал стореджу ключ и значение(то что пишется в данный момент)
 //   localStorage.setItem(STORAGE_KEY, textMassage);
 // }
 // получаем значение из localStorage
-// function saveText() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-//   // нам надо написать если наша savedMessage приводится к true то мы можем работать и значит там есть какие-то данные(которые введены были до этого)
-//   if (savedMessage) {
-//     //   обновляем DOM , берем texArea и записываем ей value
-//     refs.textArea.value = savedMessage;
-//   }
-// }
